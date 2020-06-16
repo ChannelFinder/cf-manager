3c48a2fd19437020f26eeffc9142967b61ef099b
Kunal Shroff <kunalshroff9@gmail.com>
2020-06-16 10:06:28 -0400

    an SPI for processing pv names

---
+++ src/main/java/gov/bnl/channelfinder/report/ProcessPVNames.java
@@ -0,0 +1,10 @@
+package gov.bnl.channelfinder.report;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Functionally code which process a set of pv names and returns a report.
+ */
+public interface ProcessPVNames extends Callable<String> {
+
+}
